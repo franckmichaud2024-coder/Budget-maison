3474e6a714a37c7ff3655e8846a9ba956676fe02
Franck Michaud <franckmichaud2024@gmail.com>
2026-05-01 18:01:38 -0400

    fix supabase publishable key

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://ouvybtnapkoegfwnrnlr.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91dnlidG5hcGtvZXFmd25ybmxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyMjkwMjIsImV4cCI6MjA5MjgwNTAyMn0.LGHI9zm_yjAAs-Zb0Kz1tDU9vUGGuojQ5MbK4Nbhzmc";
+const supabaseAnonKey = "sb_publishable_24gMBEBZM883cVDj8UTtwg_qVX1NYHv";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
