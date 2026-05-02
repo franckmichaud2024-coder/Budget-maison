87b1292b5cd241f4fec6eabcd0b07ca3e84bb062
Franck Michaud <franckmichaud2024@gmail.com>
2026-05-01 20:13:49 -0400

    Fix Supabase anon key

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://tffwdycqudtwiyrsrsmg.supabase.co";
-const supabaseKey = "TA_CLE";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmZndkeWNxdWR0d2l5cnNyc21nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczODIwNzMsImV4cCI6MjA5Mjk1ODA3M30._iBr0xnwlJn57P9WEE1ZN5S4kBmUn4vsmwqfAUDJmoo";
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
